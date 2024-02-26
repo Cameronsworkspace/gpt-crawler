05dda379a5fbbef71c2f5fc60c9ffa522cfe7d0d
Cameronsworkspace <161268331+Cameronsworkspace@users.noreply.github.com>
2024-02-25 21:24:36 -0500

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
-  outputFileName: "output.json",
+  url: "https://docs.mql4.com/",
+  match: "https://docs.mql4.com/**",
+  maxPagesToCrawl: 28,
+  outputFileName: "MQL4coding.json",
   maxTokens: 2000000,
 };
